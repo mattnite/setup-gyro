043e11b3c2f0afb006bb9baeea7bc9e5484054ab
Matthew Knight <mgk1795@gmail.com>
2021-02-14 23:22:42 -0800

    Init (#2)
    * fetching gyro
    
    * update workflow
    
    * try only running on ubuntu
    
    * simplify tar.gz extraction
    
    * remember prepared code
    
    * not trying variant rn
    
    * fix typo
    
    * using variant name
    
    * remove arg in extraction
    
    * added arg
    
    * try running gyro
    
    * add bin to path
    
    * added windows
    
    * added matrix to action
    
    * removed logs

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const actions = require('@actions/core');
+const github = require('@actions/github');
+const cache = require('@actions/tool-cache')
+const semver = require('semver');
+const os = require('os');
+const path = require('path');
 
-
-// most @actions toolkit packages have async methods
-async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+async function resolveLatest(version) {
+  return '0.1.0';
 }
 
-run();
+async function downloadGyro(version) {
+  const [ os_tag, ext ] = {
+    linux: ["linux", "tar.gz"],
+    darwin: ["macos", "tar.gz"],
+    win32: ["windows", "zip"],
+  }[os.platform()];
+
+  const name = `gyro-${version}-${os_tag}-x86_64`;
+  const dl_path = await cache.downloadTool(`https://github.com/mattnite/gyro/releases/download/${version}/${name}.${ext}`);
+  const gyro_path = ext === 'zip'
+    ? await cache.extractZip(dl_path)
+    : await cache.extractTar(dl_path);
+  const bin_path = path.join(gyro_path, name, 'bin');
+  return await cache.cacheDir(bin_path, 'gyro', name)
+}
+
+async function main() {
+  let version = actions.getInput('version') || 'latest';
+  if (version === 'latest') {
+    version = await resolveLatest(version);
+  } else if (!semver.valid(version)) {
+    actions.setFailed(`${version} is an invalid version`);
+    return;
+  }
+
+  let gyro_path = cache.find('gyro', version);
+  if (!gyro_path)
+    gyro_path = await downloadGyro(version);
+
+  actions.addPath(gyro_path);
+}
+
+main().catch((err) => {
+  console.error(err.stack)
+  actions.setFailed(err.message)
+  process.exit(1)
+})
