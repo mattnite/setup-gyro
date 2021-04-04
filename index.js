a50ffbd30c1534ae2047919df182b2851ea52187
Matthew Knight <mattnite@protonmail.com>
2021-04-03 20:27:53 -0700

    add getting latest gyro (#7)
    * add getting latest gyro
    
    * add octokit
    
    * tag name fix
    
    * added log
    
    * typo

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
+const core = require('@actions/core');
 const actions = require('@actions/core');
 const github = require('@actions/github');
 const cache = require('@actions/tool-cache')
+const { Octokit } = require("@octokit/rest");
 const semver = require('semver');
 const os = require('os');
 const path = require('path');
 
-async function resolveLatest(version) {
-  return '0.1.1';
+const octokit = new Octokit()
+
+async function resolveLatest() {
+  try {
+    var releases  = await octokit.repos.listReleases({
+      owner: "mattnite",
+      repo: "gyro",
+    });
+    releases = releases.data;
+    releases = releases.filter(x => x.prerelease != true && x.draft != true);
+    if (releases.length === 0) {
+      core.setFailed("no valid releases");
+    }
+
+    return releases.reduce((max, rel) => {
+      const current = rel.tag_name
+      if (max !== null)
+        return semver.gt(current, max) ? current : max;
+      else
+        return current;
+    }, null);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 async function downloadGyro(version) {
@@ -16,6 +40,7 @@ async function downloadGyro(version) {
     win32: ["windows", "zip"],
   }[os.platform()];
 
+  core.info(`downloading gyro ${version}`);
   const name = `gyro-${version}-${os_tag}-x86_64`;
   const dl_path = await cache.downloadTool(`https://github.com/mattnite/gyro/releases/download/${version}/${name}.${ext}`);
   const gyro_path = ext === 'zip'
@@ -28,7 +53,7 @@ async function downloadGyro(version) {
 async function main() {
   let version = actions.getInput('version') || 'latest';
   if (version === 'latest') {
-    version = await resolveLatest(version);
+    version = await resolveLatest();
   } else if (!semver.valid(version)) {
     actions.setFailed(`${version} is an invalid version`);
     return;
